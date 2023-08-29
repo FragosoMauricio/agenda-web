8998928b66545807093ab02d3b82078500a006a3
Mauricio Fragoso <mauricioj.fragoso@msn.com>
2023-08-29 19:05:30 -0300

    aula29082023

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,64 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Botao from '../Botao';
+import IContato from '../../types/contato';
 
-interface IFormularioPros {
+interface IFormularioProps {
     setContatos: React.Dispatch<React.SetStateAction<IContato[]>>;
 }
 
-function Form ({setContatos}: IFormularioPros) {
-    interface IContato {
-        nome: string;
-        fone: string;
+function Form({ setContatos }: IFormularioProps) {
+
+    const [nome, setNome] = useState("");
+    const [fone, setFone] = useState("");
+
+    function adicionarContato(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        setContatos((contatos) => [...contatos, { nome, fone }]
+        );
+        setNome("");
+        setFone("");
+        const formulario = evento.target as HTMLFormElement;
+        document.getElementById("nome")?.focus();
+        formulario.reset();
     }
 
-    function salvar (event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const formulario = event.target as HTMLFormElement;
-        const dados = new FormData(formulario);
-        const contato: IContato = {
-            nome: dados.get('nome') as string
-            fone: dados.get('fone') as string
-        };
-
-        console.log(contato);
-    }
-    
-    function teste () {
-        console.log('teste');
-    }
-
-
-
-    return (        
-                    <form id="formulario-de-cadastro" onSubmit = {AdicionarContato}>
-                        <div className="mb-3">
-                            <label htmlFor="nome" className="form-label">Nome:</label>
-                            <div className="input-group">
-                                <span className="input-group-text">
-                                    <i className="fas fa-user"></i>
-                                </span>
-                                <input type="text" className="form-control" name="nome" id="nome" />
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <label htmlFor="fone" className="form-label">Fone:</label>
-                            <div className="input-group">
-                                <span className="input-group-text">
-                                    <i className="fas fa-phone"></i>
-                                </span>
-                                <input type="tel" className="form-control" name="fone" id="fone" />
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <Botao type="submit" className="btn btn-primary btn-lg">
-                                <i className="fas fa-floppy-disk"></i> Salvar
-                            </Botao>
-                            <Botao type="reset" className="btn btn-danger btn-lg me-4">
-                                <i className="fas fa-broom"></i> Limpar
-                            </Botao>
-                        </div>
-                    </form>
-                
+    return (
+        <form id="formulario-de-cadastro" onSubmit={adicionarContato}>
+            <div className="mb-3">
+                <label htmlFor="nome" className="form-label">Nome:</label>
+                <div className="input-group">
+                    <span className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </span>
+                    <input type="text" 
+                        className="form-control" 
+                        name="nome" 
+                        id="nome"
+                        onChange={evento => setNome(evento.target.value)} />
+                </div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="fone" className="form-label">Fone:</label>
+                <div className="input-group">
+                    <span className="input-group-text">
+                        <i className="fas fa-phone"></i>
+                    </span>
+                    <input type="tel" 
+                        className="form-control" 
+                        name="fone" 
+                        id="fone" 
+                        onChange={evento => setFone(evento.target.value)}/>
+                </div>
+            </div>
+            <div className="mb-3">
+                <Botao type="submit" 
+                    className="btn btn-primary btn-lg me-2">
+                    <i className="fas fa-floppy-disk"></i> Salvar
+                </Botao>
+                <Botao type="reset" 
+                    className="btn btn-danger btn-lg">
+                    <i className="fas fa-broom"></i> Limpar
+                </Botao>
+            </div>
+        </form>
     );
 }
 
